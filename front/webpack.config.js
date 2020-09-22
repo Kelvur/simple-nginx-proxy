0c335f135af1b3deb1fa3e5b2b3f126d0d673e94
Miguel Osorio @kelvur <Osorio10.92@gmail.com>
2020-09-22 23:20:04 +0200

    chore: improve the webpack config

--- front/webpack.config.js
+++ front/webpack.config.js
@@ -1,15 +1,26 @@
 // Core
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const webpack = require('webpack')
 
 
 module.exports = {
     entry: './src/index.js',
+    mode: 'development',
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js'
     },
     plugins: [
-        new HtmlWebpackPlugin({template: './public/index.html'})
-    ]
+        new HtmlWebpackPlugin({ template: './public/index.html' }),
+        new webpack.DefinePlugin({'process.env.BASE_URL': JSON.stringify(process.env.BASE_URL)})
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+        ],
+    },
 };
